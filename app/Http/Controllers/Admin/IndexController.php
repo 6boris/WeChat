791c9734bc9a4b3748085edde58adbbd7e7e5394
坠星星 <280183608@qq.com>
2017-08-21 21:09:03 +0800

    框架初始化

---
+++ app/Http/Controllers/Admin/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Tool\ValidateCode;
+
+class IndexController extends Controller
+{
+    public function index(){
+    	echo "123";
+    }
+}
